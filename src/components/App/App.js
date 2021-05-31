5212b10187b2b42254da5fadcc65851d59890e5a
Caroline Eubanks <cmeubanks@gmail.com>
2021-05-31 10:27:45 -0600

    created basic App structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Jobs from '../Jobs/Jobs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jobs: []
+    }
+  }
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Space Truckers</h1>
+        <Jobs />
+      </main>
+    );
+  }
 }
 
 export default App;
