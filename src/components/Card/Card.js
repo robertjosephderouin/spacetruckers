1c036fee27679ed14dd136a8e798eb965f741998
Marceline Ball <marchettiball@gmail.com>
2021-05-31 11:03:42 -0600

    Add card jsx

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Card () => {
-  return(
-
+const Card = ({ image, name, date, pay, fluff }) => {
+  return (
+    <div className='card'>
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <p>{date}</p>
+      <p>{pay}</p>
+      <p>{fluff}</p>
+    </div>
   )
 }
 
