60222a69ddef41ee14b57663cbdece7da693c770
Caroline Eubanks <cmeubanks@gmail.com>
2021-05-31 19:43:57 -0600

    added card booked test

--- cypress/integration/jobs-view-test.js
+++ cypress/integration/jobs-view-test.js
@@ -8,20 +8,33 @@ describe('Jobs View', () => {
     cy.visit('http://localhost:3000/')
   });
 
-  it('Should display the company name on load', () => {
-    cy.get('h1').contains('Space Truckers')
+  describe('Page Load View', () => {
+
+    it('Should display the company name on load', () => {
+      cy.get('h1').contains('Space Truckers')
+    });
+
+    it('Should display all jobs on load', () => {
+      cy.get('.job-container')
+        .find('.card').should('have.length', 4)
+    });
+
+    it('Should load individual job data into each Card', () => {
+      cy.get('.card').get('#1')
+        .get('h3').contains('Mars')
+        .get('.date').contains('11:10:2195')
+        .get('.pay').contains(50000000)
+        .get('.fluff').contains('Mars is a barren planet with little resources after it was used up by hyper capitalists in the early 21st century.')
+    });
   });
 
-  it('Should display all jobs on load', () => {
-    cy.get('.job-container')
-      .find('.card').should('have.length', 4)
-  });
+  describe('Job Booking', () => {
 
-  it('Should load individual job data into each Card', () => {
-    cy.get('.card').get('#1')
-      .get('h3').contains('Mars')
-      .get('.date').contains('11:10:2195')
-      .get('.pay').contains(50000000)
-      .get('.fluff').contains('Mars is a barren planet with little resources after it was used up by hyper capitalists in the early 21st century.')
-  })
-})
+    it('Should change the card value to booked when', () => {
+      cy.get('#book2')
+        .click()
+        .get('.card').get('#2')
+        .get('.booked').contains('Job booked')
+    });
+  });
+});
