977f3556f28cda712ba96c9721e6bbc19fc3ac84
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-03 09:53:48 -0700

    Add flex to card container

--- src/jobData.js
+++ src/jobData.js
@@ -6,7 +6,7 @@ const jobs = [
     date: '11:10:2195',
     pay: 50000000,
     isBooked: false,
-    fluff: 'Mars is a barren planet with little resources after it was used up by hyper capitalists in the early 21st century.',
+    fluff: 'A barren planet with little resources after it was used up by mega capitalists.',
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const jobs = [
     date: '6:05:2198',
     pay: 10000000,
     isBooked: false,
-    fluff: 'Jupiter is a big gas giant and you won\'t be able to land your rig on the surface.',
+    fluff: 'Its a big gas giant and you won\'t be able to land your rig on the surface.',
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const jobs = [
     date: '01:21:2234',
     pay: 100000000,
     isBooked: false,
-    fluff: 'Pluto is now a planet again and thus we can space truck too it.  Be careful its too small to land on normally so you have to space parallel park.',
+    fluff: 'Be careful its too small to land on normally so you have to space parallel park.',
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ const jobs = [
     date: '04:22:2179',
     pay: 5000000,
     isBooked: false,
-    fluff: 'Nothing will ever go wrong here, trust me! Make sure to grab all ominous eggs in the ruins of a long dead alien species, don\'t even worry about it!',
+    fluff: 'Make sure to grab all ominous eggs in the ruins of a long dead alien species.',
   },
 ]
 export default jobs
