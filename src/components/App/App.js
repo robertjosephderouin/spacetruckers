e5ab437d7a574d13447e59830eca2292c4b548d5
robertjosephderouin <robertjosephderouin@gmail.com>
2021-06-04 16:59:29 -0700

    Setup react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Jobs from '../Jobs/Jobs';
 import React, { Component } from 'react'
 import { getJobs, deleteJob, bookJob } from '../../api-calls'
+import {Route } from "react-router-dom";
 
 class App extends Component {
   constructor() {
@@ -50,9 +51,14 @@ class App extends Component {
   render() {
     return (
       <main className="App">
+        {!this.state.jobs.length && !this.state.error && <h2>Loading jobs...</h2>}
         <h1>Space Truckers</h1>
         <h2 className='subtitle'>"Where were going, there are no laws."</h2>
-        <Jobs jobs={this.state.jobs} removeJob={this.removeJob} bookThisJob={this.bookThisJob} />
+        <Route exact path="/" render={() => {
+          return (
+            <Jobs jobs={this.state.jobs} removeJob={this.removeJob} bookThisJob={this.bookThisJob} />
+          )
+        }}/>
       </main>
     );
   }
