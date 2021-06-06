e3eba07759b2dfc190519900db916ce3ffd0e575
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-06 13:15:56 -0600

    added error handling tests

--- cypress/integration/error-handling.js
+++ cypress/integration/error-handling.js
@@ -10,5 +10,20 @@ describe('Error Handling', () => {
         .contains('something went wrong')
     });
 
+    it('Should display error message for 404 status code', () => {
+      cy.intercept('https://spacetruckersapi.herokuapp.com/api/v1/jobs', {
+        statusCode: 404
+      })
+      cy.visit('http://localhost:3000/')
+        .contains('something went wrong')
+    });
+
+    it('Should display error message for 400 status code', () => {
+      cy.intercept('https://spacetruckersapi.herokuapp.com/api/v1/jobs', {
+        statusCode: 400
+      })
+      cy.visit('http://localhost:3000/')
+        .contains('something went wrong')
+    });
   });
 });
